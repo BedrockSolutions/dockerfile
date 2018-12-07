0270ddae2d054d3ba8895c56e7a28e3508d2c191
Jay Cheroske <jay@portal.org>
2018-12-07 13:46:41 -0800

    tfa-bot and tfa-bot-slim tested

--- tfa-bot/bin/publish.sh
+++ tfa-bot/bin/publish.sh
@@ -17,6 +17,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 cd ${SCRIPT_DIR}/..
 
-docker build -t ${NAMESPACE}/${IMAGE_NAME}:${TAG} -t ${NAMESPACE}/${IMAGE_NAME}:latest .
+docker build -t ${IMAGE_NAME}:latest -t ${NAMESPACE}/${IMAGE_NAME}:${TAG} -t ${NAMESPACE}/${IMAGE_NAME}:latest .
 
 docker push ${NAMESPACE}/${IMAGE_NAME}
