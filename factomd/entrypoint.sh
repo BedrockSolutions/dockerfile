ce80cd1ef4b8a67d94a9971259e12809eee55320
Jay Cheroske <jay@portal.org>
2018-12-24 03:52:44 -0800

    inotify watch on factomd.conf working

--- factomd/entrypoint.sh
+++ factomd/entrypoint.sh
@@ -2,6 +2,16 @@
 
 set -e
 
-envsubst < /home/factomd/.factom/private/factomd.conf > /home/factomd/.factom/private-conv/factomd.conf
+{
+  while true; do
+    inotifywait -m ~/.factom/private -e close_write |
+      while read path action file; do
+        echo "inotifywait: ${action} event on ${path}${file}"
+        if [ -f "~/.factom/private/factomd.conf" ] ; then
+          envsubst < ~/.factom/private/factomd.conf > ~/.factom/private-conv/factomd.conf
+        fi
+      done
+  done
+} &
 
 exec /go/bin/factomd "$@"
