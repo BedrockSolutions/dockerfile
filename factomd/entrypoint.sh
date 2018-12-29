199b89d5bb1783822cad16dbb08f701df5fe1d21
Jay Cheroske <jay@portal.org>
2018-12-28 21:25:14 -0800

    Documentation

--- factomd/entrypoint.sh
+++ factomd/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 {
   while true; do
-    inotifywait -m ~/.factom/private -e create -r |
+    inotifywait -m ~/.factom/private -e create -e modify -r |
       while read path action file; do
         echo "inotifywait: ${action} event on ${path}${file}"
         envsubst < ~/.factom/private/factomd.conf > ~/.factom/private-conv/factomd.conf
